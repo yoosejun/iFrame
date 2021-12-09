480a1ab6f596a577b53a85b051bb626f099dd567
KOREAIT <KOREAIT@DESKTOP-7URGO7L>
2021-12-09 11:32:19 +0900

    membervo update

--- iFrame/src/main/java/com/cen/member/model/MemberVo.java
+++ iFrame/src/main/java/com/cen/member/model/MemberVo.java
@@ -4,93 +4,143 @@ import java.util.Date;
 
 public class MemberVo {
 
-   private int mno = 0;
-   private String mname = "";
-   private String memail  = "";
-   private String mpassword  = "";
-   private Date mbirth  = null;
-   private String mphonenum  = "";
-   private String maddress  = "";
-   private Date mcre_date = null;
-   private Date mmod_date = null;
-   
+   private String email  = "";
+   private String name = "";
+   private String password  = "";
+   private Date birth  = null;
+   private String phonenum  = "";
+   private String address1  = "";
+   private String address2  = "";
+   private String address3  = "";
+   private Date createDate = null;
+   private Date modifyDate = null;
+   private int adminCk;
+   private int point;
    
    public MemberVo() {
       super();
       // TODO Auto-generated constructor stub
    }
-   
-   public MemberVo(int mno, String mname, String memail, String mpassword, Date mbirth, String mphonenum,
-         String maddress, Date mcre_date, Date mmod_date) {
+      
+   public MemberVo(String email, String name, String password, Date birth, String phonenum, String address1, 
+         String address2, String address3, Date createDate, Date modifyDate, int adminCk, int point) {
       super();
-      this.mno = mno;
-      this.mname = mname;
-      this.memail = memail;
-      this.mpassword = mpassword;
-      this.mbirth = mbirth;
-      this.mphonenum = mphonenum;
-      this.maddress = maddress;
-      this.mcre_date = mcre_date;
-      this.mmod_date = mmod_date;
+      this.email = email;
+      this.name = name;
+      this.password = password;
+      this.birth = birth;
+      this.phonenum = phonenum;
+      this.address1 = address1;
+      this.address2 = address2;
+      this.address3 = address3;
+      this.createDate = createDate;
+      this.modifyDate = modifyDate;
+      this.adminCk = adminCk;
+      this.point = point;
+      }
+
+   public String getEmail() {
+      return email;
    }
-   public int getMno() {
-      return mno;
+
+   public void setEmail(String email) {
+      this.email = email;
    }
-   public void setMno(int mno) {
-      this.mno = mno;
+
+   public String getName() {
+      return name;
    }
-   public String getMname() {
-      return mname;
+
+   public void setName(String name) {
+      this.name = name;
    }
-   public void setMname(String mname) {
-      this.mname = mname;
+
+   public String getPassword() {
+      return password;
    }
-   public String getMemail() {
-      return memail;
+
+   public void setPassword(String password) {
+      this.password = password;
    }
-   public void setMemail(String memail) {
-      this.memail = memail;
+
+   public Date getBirth() {
+      return birth;
    }
-   public String getMpassword() {
-      return mpassword;
+
+   public void setBirth(Date birth) {
+      this.birth = birth;
    }
-   public void setMpassword(String mpassword) {
-      this.mpassword = mpassword;
+
+   public String getPhonenum() {
+      return phonenum;
    }
-   public Date getMbirth() {
-      return mbirth;
+
+   public void setPhonenum(String phonenum) {
+      this.phonenum = phonenum;
    }
-   public void setMbirth(Date mbirth) {
-      this.mbirth = mbirth;
+
+   public String getAddress1() {
+      return address1;
    }
-   public String getMphonenum() {
-      return mphonenum;
+
+   public void setAddress1(String address1) {
+      this.address1 = address1;
    }
-   public void setMphonenum(String mphonenum) {
-      this.mphonenum = mphonenum;
+
+   public String getAddress2() {
+      return address2;
    }
-   public String getMaddress() {
-      return maddress;
+
+   public void setAddress2(String address2) {
+      this.address2 = address2;
    }
-   public void setMaddress(String maddress) {
-      this.maddress = maddress;
+
+   public String getAddress3() {
+      return address3;
    }
-   public Date getMcre_date() {
-      return mcre_date;
+
+   public void setAddress3(String address3) {
+      this.address3 = address3;
    }
-   public void setMcre_date(Date mcre_date) {
-      this.mcre_date = mcre_date;
+
+   public Date getCreateDate() {
+      return createDate;
    }
-   public Date getMmod_date() {
-      return mmod_date;
+
+   public void setCreateDate(Date createDate) {
+      this.createDate = createDate;
    }
-   public void setMmod_date(Date mmod_date) {
-      this.mmod_date = mmod_date;
+
+   public Date getModifyDate() {
+      return modifyDate;
    }
+
+   public void setModifyDate(Date modifyDate) {
+      this.modifyDate = modifyDate;
+   }
+
+   public int getAdminCk() {
+      return adminCk;
+   }
+
+   public void setAdminCk(int adminCk) {
+      this.adminCk = adminCk;
+   }
+
+   public int getPoint() {
+      return point;
+   }
+
+   public void setPoint(int point) {
+      this.point = point;
+   }
+
    @Override
    public String toString() {
-      return "MemberVo [mno=" + mno + ", mname=" + mname + ", memail=" + memail + ", mpassword=" + mpassword
-            + ", mbirth=" + mbirth + ", mphonenum=" + mphonenum + ", maddress=" + maddress + ", mcre_date="
-            + mcre_date + ", mmod_date=" + mmod_date + "]";
+      return "MemberVo [email=" + email + ", name=" + name + ", password=" + password + ", birth=" + birth
+            + ", phonenum=" + phonenum + ", address1=" + address1 + ", address2=" + address2 + ", address3="
+            + address3 + ", createDate=" + createDate + ", modifyDate=" + modifyDate + ", adminCk=" + adminCk
+            + ", point=" + point + "]";
    }
+
 }
